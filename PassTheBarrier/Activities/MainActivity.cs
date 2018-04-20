546e00b491da1637740096abe739bc0bd424197b
vtrenea <vtrenea@pitechnologies.ro>
2018-04-20 10:31:28 +0300

    Lock screen orientation as portrait.

--- PassTheBarrier/Activities/MainActivity.cs
+++ PassTheBarrier/Activities/MainActivity.cs
@@ -28,7 +28,9 @@ namespace PassTheBarrier.Activities
 
 		protected override void OnCreate(Bundle bundle)
         {
-            base.OnCreate(bundle);
+	        this.RequestedOrientation = ScreenOrientation.Portrait;
+
+			base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.MainView);
